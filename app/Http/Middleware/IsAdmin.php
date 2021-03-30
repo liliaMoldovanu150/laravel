599f84f6cf16f6c669f054b1bb2a282274a702e9
Lilia Moldovanu <lilia.moldovanu@5psolutions.eu>
2021-03-30 10:39:39 +0300

    {#14} {login} {http://pm.5psolutions.net/public/index.php?path_info=projects/training-lilia-moldovanu/tasks/14} This commit will update login functionality

--- app/Http/Middleware/IsAdmin.php
+++ app/Http/Middleware/IsAdmin.php
@@ -20,7 +20,6 @@ class IsAdmin
         if (Auth::user() &&  Auth::user()->is_admin == 1) {
             return $next($request);
         }
-
         return redirect(route('product.index'));
     }
 }
