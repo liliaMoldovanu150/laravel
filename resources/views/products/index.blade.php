1c9991dad5164cd24984987e16a6166dd8002eca
Lilia Moldovanu <lilia.moldovanu@5psolutions.eu>
2021-04-13 15:41:43 +0300

    {#12} {index} {http://pm.5psolutions.net/public/index.php?path_info=projects/training-lilia-moldovanu/tasks/12} This commit will fix bugs from the review of index page

--- resources/views/products/index.blade.php
+++ resources/views/products/index.blade.php
@@ -3,27 +3,25 @@
 @section('content')
 
     <div class="content-wrapper">
-        @if ($products->count())
-            @foreach ($products as $product)
-                <div class="product-item">
-                    <div class="product-image">
-                        <img src="./images/{{ $product->image_url }}" alt="{{ __('labels.product_image') }}">
-                    </div>
-                    <div class="product-features">
-                        <div>{{ $product->title }}</div>
-                        <div>{{ $product->description }}</div>
-                        <div>{{ number_format($product->price, 2) }}</div>
-                    </div>
-                    <form action="{{ route('cart.store', $product->id) }}" method="POST">
-                        @csrf
-                        <input type="submit" value="{{ __('labels.add') }}">
-                    </form>
+        @forelse ($products as $product)
+            <div class="product-item">
+                <div class="product-image">
+                    <img src="./images/{{ $product->image_url }}" alt="{{ __('labels.product_image') }}">
                 </div>
-                <br>
-            @endforeach
-        @else
+                <div class="product-features">
+                    <div>{{ $product->title }}</div>
+                    <div>{{ $product->description }}</div>
+                    <div>{{ number_format($product->price, 2) }}</div>
+                </div>
+                <form action="{{ route('cart.store', $product->id) }}" method="POST">
+                    @csrf
+                    <input type="submit" value="{{ __('labels.add') }}">
+                </form>
+            </div>
+            <br>
+        @empty
             <p class="message">{{ __('labels.all_added') }}</p>
-        @endif
+        @endforelse
         <a class="go" href="{{ route('cart.show') }}">{{ __('labels.go_to_cart') }}</a>
     </div>
 
